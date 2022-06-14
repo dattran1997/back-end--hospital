854bb2965f96a107c5ce736d612b8452a8622ba4
dattran <38274883+dattran1997@users.noreply.github.com>
2022-06-15 00:46:35 +0700

    cors fix

--- src/server.js
+++ src/server.js
@@ -13,7 +13,16 @@ let app = express();
 
 // config app
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    const allowedHost = 'http://localhost:3000';
+
+    res.header('Access-Control-Allow-Origin', allowedHost);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
 
 viewEngine(app);
 initWebRoutes(app);
